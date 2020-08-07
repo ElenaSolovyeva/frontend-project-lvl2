d4c6de9c11f7116d4f4ac532b81a5656752904e0
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-07 17:22:15 +0200

    Creates a help option using commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-v, --version', 'output the version number')
+  .option('-h, --help', 'output usage information');
+
+// "Эта строчка приведена в документации без пояснений; непонятно, что такое process и argv"
+// Есть такое: "The options can be accessed as properties on the Command object.", но что это за объект?
+// Напрашивается мысль, что process - это объект, а argv - значения аргументов, но что это за аргументы?
+program.parse(process.argv);
+
+if (program.version) console.log()
+
+
+
 console.log('Здесь должна быть информация...');
