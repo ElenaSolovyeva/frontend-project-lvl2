755832838d14ffbd08bb642cf0d1a3179acd3799
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-07 17:30:10 +0200

    Creates a help option using commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-v, --version', 'output the version number')
+  .option('-V, --version', 'output the version number')
   .option('-h, --help', 'output usage information');
 
 // "Эта строчка приведена в документации без пояснений; непонятно, что такое process и argv"
