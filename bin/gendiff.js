db41f7d8c42e632d0e8b8c72ed7acc925b262cf8
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-08 12:27:24 +0200

    Add path arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,12 +3,19 @@
 import program from 'commander';
 
 let outputFormat;
+let pathFile1;
+let pathFile2;
+
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format' )
-  .arguments('<type>')
-  .action((type) => outputFormat = type);
+  .arguments('<type> <path1> <path2>')
+  .action((type, path1, path2) => {
+    outputFormat = type;
+    pathFile1 = path1;
+    pathFile2 = path2;
+  });
 
 program.parse(process.argv);
 
