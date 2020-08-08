dcdfc66a0b68ac5e8da6e9c876183768543aa8a6
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-08 11:59:41 +0200

    Save gendiff.js into bin directory

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+
+let outputFormat;
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<type>')
+  .action((type) => outputFormat = type);
+
+program.parse(process.argv);
+
+if (typeOf outputFormat === 'undefined') {
+  console.error('no output format given!');
+}
+
+console.log(program.args);
