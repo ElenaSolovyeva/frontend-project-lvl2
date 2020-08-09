52b315e43b57866186524802248360aca986f8b2
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-09 16:37:05 +0200

    Realise Implement a comparison of the properties of two files

---
+++ src/getFilesDifference.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const getFilesDifference = (file1Properties, file2Properties) => {
+  // Создать массивы из ключей 2х объектов, объединить, отсортировать
+  const keys1 = Object.keys(file1Properties);
+  const keys2 = Object.keys(file2Properties);
+
+  const resultKeys = _.difference(keys1, keys2).concat(keys2).sort();
+
+  // Применить reduce, аккумулятор будет объектом,
+  // заносить в несго пары ключ-значения с обработкой, одинаковые значения или нет
+  // с добавлением "+" и "-" к названию ключа
+  const result = resultKeys.reduce((acc, key) => {
+    if (!keys1.includes(key)) {
+      acc[`+${key}`] = file2Properties[key];
+    }
+
+    if (!keys2.includes(key)) {
+      acc[`-${key}`] = file1Properties[key];
+      return acc;
+    }
+
+    if (file1Properties[key] === file2Properties[key]) {
+      acc[key] = file1Properties[key];
+      return acc;
+    }
+
+    acc[`-${key}`] = file1Properties[key];
+    acc[`+${key}`] = file2Properties[key];
+    return acc;
+
+  }, {})
+
+  return result;
+};
+
+export default getFilesDifference;
