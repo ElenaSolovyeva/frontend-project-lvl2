4b96b5ff4fd8d0e034ff418170bb3bc8eda9c532
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-09 13:42:50 +0200

    Change the order of arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,11 +10,11 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format' )
-  .arguments('<type> <path1> <path2>')
-  .action((type, path1, path2) => {
-    outputFormat = type;
+  .arguments('<path1> <path2> <type>')
+  .action((path1, path2, type) => {
     pathFile1 = path1;
     pathFile2 = path2;
+    outputFormat = type;
   });
 
 program.parse(process.argv);
