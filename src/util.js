96c50e9e2a8172496732495239eedd479f6ccace
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-11 14:40:22 +0200

    Add eslint

--- src/util.js
+++ src/util.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import program from 'commander';
 import fs from 'fs';
 
@@ -7,11 +5,11 @@ const util = () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'output format' )
+    .option('-f, --format <type>', 'output format')
     .arguments('<path1> <path2> <type>')
     .action((path1, path2, type) => {
-      const outputFormat = type;
-      // Нужно получить содержимое файлов на основании их адресов
+      console.log(type); // В задании требуется указывать type в параметрах,
+      // но пока он нигде не используется --> eslint ругается
       const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
       const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
       const keys1 = Object.keys(obj1);
@@ -24,9 +22,8 @@ const util = () => {
         const [newValue, oldValue] = ['+', '-'];
 
         if (!keys1.includes(key)) {
-          acc = `${acc}
+          return `${acc}
           ${newValue}${key}: ${obj2[key]}`;
-          return acc;
         }
 
         if (!keys2.includes(key)) {
@@ -49,34 +46,6 @@ const util = () => {
     });
 
   program.parse(process.argv);
-
-  // if (typeof outputFormat === 'undefined') {
-  //   console.error('no output format given!');
-  // }
-
-  // console.log(program.args);
 };
 
 export default util;
-
-
-// Получить на основании параметров с адресами 2х файлов два файла JSON,
-// содержащих свойства сравниваемых файлов
-
-// Опарсить каждый из 2х полученных JSON-файлов, присвоить их свойства объектам
-
-// На данном этапе использую готовые объекты
-// const file1Properties = {
-//   "host": "hexlet.io",
-//   "timeout": 50,
-//   "proxy": "123.234.53.22",
-//   "follow": false
-// };
-//
-// const file2Properties = {
-//   "timeout": 20,
-//   "verbose": true,
-//   "host": "hexlet.io"
-// };
-
-//console.log(getFilesDifference(file1Properties, file2Properties));
