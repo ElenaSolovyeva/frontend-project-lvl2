f9595361bd215e3bc118e0bba4f7eae8f0c76bab
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-12 16:07:08 +0200

    -

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,34 +1,8 @@
 // Запуск с тестом  $ node tests/capitalize.test.js
+import util from '../src/util.js';
 
-// Достаточно написать ровно одну проверку, которая покрывает основной сценарий.
-// Дальше нужно смотреть на "пограничные случаи":
-//  - Работа с пустой строкой;
-//  - Обработка null;
-//  - Деление на ноль (в большинстве языков вызывает ошибку)
-//  - Специфические ситуации для конкретных алгоритмов.
+const result = '{-follow: false   host: hexlet.io  -proxy: 123.234.53.22  -timeout: 50  +timeout: 20  +verbose: true}';
+const path1 = '/mnt/c/Projects/frontend-project-lvl2/src/file1.json';
+const path2 = '/mnt/c/Projects/frontend-project-lvl2/src/file2.json';
 
-// Ошибки типов входных данных тестировать не нужно!
-
-// ***** Утверждения (Asserts) *****
-
-// import { strict as assert } from 'assert';
-// import util from '../src/util.js';
-//
-// assert(util() === {});
-
-// Специализированные утверждений, заточенные под конкретные ситуации:
-//  - assert.strictEqual(actual, expected)
-//    подходит при сравнении двух значений; проверяет равенство ПО ССЫЛКЕ
-//    assert.strictEqual(capitalize('hello'), 'Hello');
-
-//  - assert.deepStrictEqual(actual, expected) - для сравнения ПО ЗНАЧЕНИЮ
-//    assert.deepStrictEqual({ key: 'value' }, { key: 'value' });
-
-// ************* Библиотека power-assert **********
-
-// import assert from 'power-assert';
-// import util from '../src/util.js';
-//
-// assert(util() === {});
-
-// л
+test('show difference', () => expect(util(path1, path2, 'txt')).toBe(result));
--- tests/util.test.js
+++
@@ -1,8 +0,0 @@
-// Запуск с тестом  $ node tests/capitalize.test.js
-import util from '../src/util.js';
-
-const result = '{-follow: false   host: hexlet.io  -proxy: 123.234.53.22  -timeout: 50  +timeout: 20  +verbose: true}';
-const path1 = '/mnt/c/Projects/frontend-project-lvl2/src/file1.json';
-const path2 = '/mnt/c/Projects/frontend-project-lvl2/src/file2.json';
-
-test('show difference', () => expect(util(path1, path2, 'txt')).toBe(result));
