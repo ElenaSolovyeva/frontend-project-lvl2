19c539a0c4533d22a615c0f2a3ee056b6dab9928
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-18 14:47:44 +0200

    Fix test.util.js

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,16 +1,17 @@
+import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-import util from '../src/util.js';
+import getDifference from '../src/getDifference.js';
 
 const __filename = fileURLToPath(import.meta.url);
-console.log(`__filename: ${__filename}`);
 const __dirname = dirname(__filename);
-console.log(`__dirname: ${__dirname}`);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const path1 = getFixturePath('file1.json');
 const path2 = getFixturePath('file2.json');
-const result = getFixturePath('result.json');
+const pathResult = getFixturePath('result.txt');
+const result = fs.readFileSync(pathResult, 'utf8');
 
-test('show difference', () => expect(util(path1, path2, 'txt')).toBe(result));
+test('show difference', () => expect(getDifference(path1, path2, 'txt'))
+  .toBe(result));
