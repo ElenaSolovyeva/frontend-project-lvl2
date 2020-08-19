0501ca1bcd1b78da8a46808e9fa1208a410386fe
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-19 18:09:34 +0200

    Add the opportunity to compare .yml-file with another .yml- or .json-file

--- src/getDifference.js
+++ src/getDifference.js
@@ -1,8 +1,29 @@
 import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 
 const getDifference = (path1, path2) => {
-  const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
-  const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
+  const typeOfFile1 = path.extname(path1);
+  const typeOfFile2 = path.extname(path2);
+  let obj1;
+  let obj2;
+
+  if (typeOfFile1 === '.json') {
+    obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
+  } else if (typeOfFile1 === '.yml') {
+    obj1 = yaml.safeLoad(fs.readFileSync(path1, 'utf8'));
+  } else {
+    return 'Unknown extantion of file1';
+  }
+
+  if (typeOfFile2 === '.json') {
+    obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
+  } else if (typeOfFile2 === '.yml') {
+    obj2 = yaml.safeLoad(fs.readFileSync(path2, 'utf8'));
+  } else {
+    return 'Unknown extantion of file2';
+  }
+
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const resultKeys = keys1.filter((key) => !keys2.includes(key))
