b158547ace2cee5eef9b4f98b2078d51d16cab7a
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-19 19:40:04 +0200

    Install ini

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,25 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parse = (filePath) => {
   const typeOfFile = path.extname(filePath);
 
-  return (typeOfFile === '.yml')
-    ? yaml.safeLoad(fs.readFileSync(filePath, 'utf8'))
-    : JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  switch (typeOfFile) {
+    case ('.json'):
+      return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    case ('.yml'):
+      return yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
+    case ('.ini'):
+      return ini.parse(fs.readFileSync(filePath, 'utf8'));
+    default:
+      return null;
+  }
+
+  // return (typeOfFile === '.yml')
+  //   ? yaml.safeLoad(fs.readFileSync(filePath, 'utf8'))
+  //   : JSON.parse(fs.readFileSync(filePath, 'utf8'));
 };
 
 export default parse;
