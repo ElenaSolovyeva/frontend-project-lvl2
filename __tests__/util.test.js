6e214d99fa56b4cb879e6e6cbf202b1aa5d78838
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-08-19 12:28:00 +0200

    Install js-yaml

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,4 +1,4 @@
-// Первым делом реализуйте тесты, описывающие требования к функциональности
+// ЗАДАНИЕ: Первым делом реализуйте тесты, описывающие требования к функциональности
 // (то, как должно работать сравнение файлов).
 
 // Создаем два файла с исходными данными (для 5го шага проекта это file3.yml file4.yml);
@@ -6,7 +6,11 @@
 // В фале с тестом получаем пути к этим трем файлам, используя __filename и __dirname;
 // Считываем содержание файла result.txt;
 // Выполняем сравнение результата тестируемой функции (getDifference)
-//    с ожидаемым результатом (содержанием файла result.txt)
+// с ожидаемым результатом (содержанием файла result.txt):
+// - для двух json-файлов;
+// - для двух yaml-файлов;
+// - сравнение json-файла с yaml-файлом;
+// - сравнение yaml-файла с json-файлом;
 
 import fs from 'fs';
 import path, { dirname } from 'path';
@@ -18,9 +22,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const path1 = getFixturePath('file3.yml');
-const path2 = getFixturePath('file4.yml');
+const path1 = getFixturePath('file1.json');
+const path2 = getFixturePath('file2.json');
+const path3 = getFixturePath('file3.yml');
+const path4 = getFixturePath('file4.yml');
 const pathResult = getFixturePath('result.txt');
 const result = fs.readFileSync(pathResult, 'utf8').trim();
 
-test('show difference', () => expect(getDifference(path1, path2, 'txt')).toBe(result));
+test('show difference json-json', () => expect(getDifference(path1, path2, 'txt')).toBe(result));
+test('show difference yaml-yaml', () => expect(getDifference(path3, path4, 'txt')).toBe(result));
+test('show difference json-yaml', () => expect(getDifference(path1, path4, 'txt')).toBe(result));
+test('show difference yaml-json', () => expect(getDifference(path3, path2, 'txt')).toBe(result));
